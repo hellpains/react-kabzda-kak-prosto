8b85ea19627e40da2b0e57a834499733b56b1e78
Рустам Садулаев <rustam2004sadulaev@mail.ru>
2022-03-24 05:01:19 +0300

    'lesson-2'

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,66 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    console.log('App rendering')
     return (
         <div className="App">
-            This is APP component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
 }
 
+const AppTitle = () => {
+    console.log('AppTitle rendering')
+    return <>This is APP component</>
+}
+
 
 const Rating = () => {
-  return(
-      <div>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
-      </div>
-  )
+    console.log('Rating rendering')
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
 }
 
 const Star = () => {
-  return(
-      <div>star</div>
-  )
+    console.log('Star rendering')
+    return (
+        <div>star</div>
+    )
 }
 
 
 const Accordion = () => {
-  return(
-      <div>
-          <h3>Menu</h3>
-          <ul>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-          </ul>
-      </div>
-  )
+    console.log('Accordion rendering')
+    return (
+        <div>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </div>
+    )
 }
-
+const AccordionTitle = () => {
+    console.log('AccordionTitle rendering')
+    return <h3>Menu</h3>
+}
+const AccordionBody = () => {
+    console.log('AccordionBody rendering')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
 
 export default App;
