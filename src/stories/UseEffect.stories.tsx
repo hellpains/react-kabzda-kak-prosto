157ecf4b240c22d4fba8d1fb6db6046d65f2ead7
hellpains <rustam2004sadulaev@mail.ru>
2022-05-08 03:07:45 +0300

    '20.useEffect.setTimeout.setInterval'

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -10,29 +10,82 @@ export const SimpleExample = () => {
 
     console.log('SimpleExample')
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect every render')
-        document.title=counter.toString()
+        document.title = counter.toString()
     })
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect only first render (componentDidMount)')
-        document.title=counter.toString()
-    },[])
+        document.title = counter.toString()
+    }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect first and every counter change')
-        document.title=counter.toString()
-    },[counter])
+        document.title = counter.toString()
+    }, [counter])
 
 
     return (
         <>
             Hello, {counter} {fake}
-            <button onClick={() => {setCounter(counter + 1)}}>+</button>
-            <button onClick={() => {setFake(fake + 1)}}>+</button>
+            <button onClick={() => {
+                setCounter(counter + 1)
+            }}>+
+            </button>
+            <button onClick={() => {
+                setFake(fake + 1)
+            }}>+
+            </button>
         </>
     )
 }
 
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(0);
+    const [fake, setFake] = useState(0);
+
+    console.log('SetTimeoutExample')
+
+
+    useEffect(() => {
+
+        setInterval(() => {
+            setCounter(state=>state+1)
+        }, 1000)
+
+    }, [])
+
+    return (
+        <>
+            <>
+                Hello, counter: {counter} fake: {fake}
+                {/*<button onClick={() => {setCounter(counter + 1)}}>+</button>*/}
+                {/*<button onClick={() => {setFake(fake + 1)}}>+</button>*/}
+            </>
+        </>
+    )
+}
+
+
+export const Timer = () => {
+    const [timer, setTimer] = useState<Date>();
+
+    console.log('SetTimeoutExample')
+
+
+    useEffect(() => {
+
+        setInterval(() => {
+            setTimer(new Date())
+        }, 1000)
+
+    }, [])
+
+    return (
+        <>
+            Time: {timer?.toLocaleTimeString()}
+        </>
+    )
+}
 
